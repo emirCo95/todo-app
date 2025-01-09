1f44897c0be27b57b4abc7b92a82a06936a09d42
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-01-09 11:18:23 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
+import { useState, useRef } from 'react';
+
 const App = () => {
+  const [taskList, setTaskList] = useState([]);
+
+  const inputRef = useRef();
+
+  const handleAddTask = () => {
+    const newTask = inputRef.current.value;
+    setTaskList((prevTasks) => {
+      return [...prevTasks, newTask];
+    });
+  };
+
   return (
     <div className="w-full h-[100vh] flex flex-col justify-center items-center">
       <div className="w-[500px] flex gap-2">
         <input
+          ref={inputRef}
           type="text"
+          placeholder="Write to-do task here..."
           className="border-2 border-gray-500 w-[300px] h-[50px] rounded-md text-xl p-2"
         />
-        <button className="w-[150px] rounded-md p-3 bg-black text-white">
+        <button
+          onClick={handleAddTask}
+          className="w-[150px] rounded-md p-3 bg-black text-white"
+        >
           Add Task
         </button>
       </div>
-      <div className="w-[500px] flex justify-start gap-2">
-        <div className="w-[300px] h-[50px] bg-gray-200 flex justify-center items-center my-5 rounded-md">
-          Do laundry
-        </div>
-        <button className="h-[50px] w-[150px] rounded-md p-3 bg-black text-white my-5">
-          Delete
-        </button>
-      </div>
+      {taskList.map((task, idx) => {
+        return (
+          <div key={idx} className="w-[500px] flex justify-start gap-2 my-1">
+            <div className="w-[300px] h-[50px] bg-gray-200 flex justify-start px-2 items-center rounded-md text-xl">
+              {task}
+            </div>
+            <button className="h-[50px] w-[150px] rounded-md p-3 bg-black text-white">
+              Delete
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 };
